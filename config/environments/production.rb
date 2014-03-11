17e6c67dfac8513933bb4ca9cb64fa8c4529e542
unknown <wbsurfver@gmail.com>
2014-03-11 18:02:34 -0400

    disable asset pipeline in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ CloudCare::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.assets.enabled = false
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
